67f256cf85a2fb08087432cc73a2e0c2479005bf
Aeizzz <1129603149@qq.com>
2018-06-12 21:17:58 +0800

    增加bean转为 map 修改map转为bean 可处理对象嵌套类型 为完成列表处理

--- src/edu/wfu/utils/BeanUtils.java
+++ src/edu/wfu/utils/BeanUtils.java
@@ -5,6 +5,7 @@ import java.beans.IntrospectionException;
 import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.Map;
 
 public class BeanUtils {
@@ -29,8 +30,15 @@ public class BeanUtils {
                 String name = entry.getKey();
                 Object value = entry.getValue();
                 if (isWriteable(target, name)) {
-                    copyProperty(target, name, value);
-
+                    // 如果值是Map 的话则有嵌套
+                    if (value instanceof Map) {
+                        // 获取 对应的对象递归赋值
+                        Object temp = getObgect(target, name);
+                        copyProperties(value, temp);
+                        copyProperty(target, name, temp);
+                    } else {
+                        copyProperty(target, name, value);
+                    }
                 }
             }
         } else {
@@ -56,6 +64,70 @@ public class BeanUtils {
 
     }
 
+    private static Object getObgect(Object target, String name) throws Exception {
+        BeanInfo info = Introspector.getBeanInfo(target.getClass());
+        PropertyDescriptor[] targetDescriptors =
+                info.getPropertyDescriptors();
+
+        for (PropertyDescriptor pd : targetDescriptors) {
+            if (pd.getName().equals(name)) {
+                // 根据类型来new 出一个新的对象
+                String type = pd.getPropertyType().getName();
+                Class t = Class.forName(type);
+                Object value = t.newInstance();
+
+//                System.out.println(value.getClass().toString());
+                return value;
+            }
+        }
+
+        throw new Exception("没有键" + name);
+
+    }
+
+
+    public static void copyProperties(Object source, Map<String, Object> target) throws Exception {
+        if (source == null) {
+            throw new Exception("源对象为空");
+        }
+
+        if (target == null) {
+            throw new Exception("目标对象为空");
+        }
+        if (source instanceof Map) {
+            Map<String, Object> propMap = (Map<String, Object>) source;
+            for (Map.Entry entry : propMap.entrySet()) {
+                target.put((String) entry.getKey(), entry.getValue());
+            }
+
+        } else {
+            BeanInfo info = Introspector.getBeanInfo(source.getClass());
+            PropertyDescriptor[] sourceDescriptors =
+                    info.getPropertyDescriptors();
+            for (PropertyDescriptor pd : sourceDescriptors) {
+                String name = pd.getName();
+                if ("class".equals(name)) {
+                    continue;
+                }
+                Method method = pd.getReadMethod();
+                Object value = method.invoke(source);
+                String type = pd.getPropertyType().getName();
+//                System.out.println(type);
+                // 判断是否是基础类型,如果不是则递归去转化
+                if (!(type.contains("java.lang") || type.contains("boolean") || type.contains("int")
+                        || type.contains("double") || type.contains("long") || type.contains("char")
+                        || type.contains("byte") || type.contains("short") || type.contains("float"))) {
+                    Map<String, Object> temp = new HashMap<>();
+                    copyProperties(value, temp);
+                    target.put(name, temp);
+                } else {
+                    target.put(name, value);
+                }
+            }
+        }
+
+    }
+
 
     /**
      * 复制属性到目标对象
@@ -77,7 +149,6 @@ public class BeanUtils {
                         // TODO 处理整数类型
                         method.invoke(target, value);
                     } else {
-
                         method.invoke(target, value);
                     }
                     return;
@@ -99,10 +170,10 @@ public class BeanUtils {
      * @return
      * @throws IntrospectionException
      */
-    private static boolean isWriteable(Object bean, String name) throws IntrospectionException {
+    private static boolean isWriteable(Object bean, String name) throws Exception {
 
         if (name == null) {
-
+            throw new Exception("键不能为null");
         }
 
         BeanInfo info = Introspector.getBeanInfo(bean.getClass());
